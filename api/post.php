56b41a266eee1dd4a0bc604348cdf76cd28c39ee
igsousab <igortthq@gmail.com>
2020-04-03 11:47:58 -0300

    Commiting untracked backups 1

--- api/post.php
+++ api/post.php
@@ -1,46 +1,30 @@
-<?php
+<?php 
+//headers??
+
+//Define param banco
+$con = mysqli_connect('localhost','igor','0321grade','api_db');
+//muda charset
+$con->set_charset("utf8");
 
 
-include_once 'resources/funcoes.php';
+$post = json_decode(file_get_contents('php://input'));
 
 
-//
-// BANCO DE DADOS
-//
+if($con->connect_error) die("ERRO CONEXÂO BANCO: ".$con->connect_error); //Caso não aconteça conexão com o banco
 
-	$conn = pg_connect("host=localhost dbname=meuhorario_dev user=meuhorario password=123456"); // Conecta banco de dados
-	pg_set_client_encoding ([$conn], "utf8" ); 	// muda charset
-	
+else{
+    $result = mysqli_query($con, "SELECT * FROM `products`"); //Puxa dados da tabela selecionada
+    
+    echo "<h1> Minha Linda Tabela </h1>";
+    while($rowData = mysqli_fetch_array($result)){ //Força um loop com os dados selecionados no SQL acima
+        echo "<p style='color: #FF08E3;'>";
+        echo $rowData[1];
+        echo "</p>";
+        echo "<p style='color: #FF08E3;'>";
+        echo $rowData[2];
+        echo "</p>";
+    }
+    echo "<p>*******Meu Lindo Fim********</p>";
+}
 
-// Testar conexão com o Banco if die
-
-//
-// RECEBE VARIÁVEIS POST
-//
-
-	$name = $_POST['name'];
-	$table = $_POST['table'];
-	$description = $_post['description'];
-
-
-//
-// FAZ A REQUISIÇÃO
-//
-
-
-
-	if( 	// Se não está vazio
-		!empty($table)
-	){
-
-		$result = pg_query($conn, "INSERT INTO $table (name, description) VALUES ('$name', '$description')");
-		echo $result;
-
-		if (!$result){
-
-			echo "query failed!";
-
-		}
-
-
-	}
+?>
--- api/resources/funcoes.php
+++ api/resources/funcoes.php
@@ -1,7 +1,5 @@
 <?php
 
-//	TRANSFORM DAY END AND DAY START IN AN ARRAY OF EACH DAY
-
 	function make_days_query($days){
 
 
@@ -15,107 +13,6 @@
 		return $query;
 
 	}
-
-	function conflict($schedule_data, $DAY){
-
-
-		for($i = 0; $i < count($days); $i++){ // FOR EACH DAY
-
-			$match = 0;
-
-			for($j = 0; $j < count($days); $j++){ // CHECK IF DISCIPLINE DAY MATCH ANY DAY IN THE GIVEN SCHEDULE 
-
-				if($schedule_data[$j][day] == $days[$j]){
-					$match = 1;
-					break;
-
-				}
-			} 
-
-			if($match == 0){
-
-				return 1;
-
-			}
-
-
-	
-			if( $schedule_data[$i][end_hour] <=  $day_start[0] ){  // IF DISCIPLINE ENDS BEFORE STUDENT LIMITS
-
-				return 1;
-
-			} else if( $schedule_data[$i][end_hour] ==  $day_start[0] && $schedule_data[$i][end_minute] <= $day_start[1] ){
-
-				return 1;
-
-			}
-
-
-			echo $schedule_data[$i][end_hour] . ":" . $schedule_data[$i][end_minute] .  " > " . $day_start[0] . ":" . $day_start[1] . "<br>";
-
-			
-/*
-if($schedule_data[$i][start_hour] >=  $day_end[0] ){ // IF THE DISCIPLINE STARTS AFTER STUDENT LIMITS
-
-	return 1;
-
-} else if( $schedule_data[$i][start_hour] ==  $day_end[0] && $schedule_data[$i][start_minute] > $day_end[1] ){
-	 
-	return 1;
-}
-*/
-
-
-			if( !empty($lunch) ){ // IF THERE IS LUNCH TIME
-
-// IF LUNCH INTERVAL IS BIGGER THAN 2 HOURS AND DISCIPLINE STARTS AT 13:00
-
-				if ($lunch > 120 && $schedule_data[$i][start_hour] == 13 && $schedule_data[$i][start_minute] == 00){ 
-
-					return 1; // CONFLICT
-
-// IF LUNCH INTERVAL IS BIGGER THAN 1:30 HOURS AND DISCIPLINE ENDS AT 12:30
-
-				} else if($lunch > 90 && $schedule_data[$i][end_hour] == 12 && $schedule_data[$i][end_minute] == 30){
-
-					return 1;
-
-				}				
-
-
-			}
-
-
-
-			if(!empty($dinner) ){ // IF THERE IS DINNER TIME
-// IF DINNER INTERVAL IS BIGGER THAN 2:20 HOURS AND DISCIPLINE ENDS AT 16:40
-
-				if ($dinner > 140 && $schedule_data[$i][end_hour] == 16 && $schedule_data[$i][end_minute] == 40){
-
-					return 1; // CONFLICT
-
-// IF DINNER INTERVAL IS BIGGER THAN 30 MINUTES AND DISCIPLINE ENDS AT 18:30
-				} else if($dinner > 30 && $schedule_data[$i][end_hour] == 18 && $schedule_data[$i][end_minute] == 30){ 
-
-					return 1;
-
-
- // IF DINNER INTERVAL IS BIGGER THAN 1:30 HOURS AND DISCIPLINE STARTS AT 18:30
-				} else if($dinner > 90 && $schedule_data[$i][start_hour] == 18 && $schedule_data[$i][start_minute] == 30){ 
-
-					return 1;
-
-				}
-
-			}
-
-
-		}
-
-		return 0;
-	}
-
-
 	
 	function decide_day($day_number){
 
@@ -161,19 +58,15 @@ if($schedule_data[$i][start_hour] >=  $day_end[0] ){ // IF THE DISCIPLINE STARTS
 		if(!$result){	// Se não houve resultado
 
 			echo $query . " did not execute";
-			return 0;
 
 		} else	if(pg_num_rows($result) == 0){ // Se não há registros
 
-			echo "<br></br>0 records of " . $query . "<br></br>";
-			return 0;
+			//echo "<br></br>0 records of " . $query . "<br></br>";
 
 		} else {
 
 			$data = pg_fetch_all($result);
-			
-			echo $query . " sucessfull!<br></br>";
-			
+		  //  echo $query . " sucessfull!<br></br>";
 			return $data;
 
 
@@ -181,6 +74,8 @@ if($schedule_data[$i][start_hour] >=  $day_end[0] ){ // IF THE DISCIPLINE STARTS
 		
 
 	}
+
+
 
 
 	function show_data($data){
@@ -200,8 +95,10 @@ if($schedule_data[$i][start_hour] >=  $day_end[0] ){ // IF THE DISCIPLINE STARTS
 
 		if($table == disciplines){
 
-//echo "<p style='color: #FF08E3;'>";
-//echo "Id - Nome - Código - Curriculo - Ementa <br>";
+
+
+			//echo "<p style='color: #FF08E3;'>";
+			//echo "Id - Nome - Código - Curriculo - Ementa <br>";
 
 			echo $row[id] . "   -   " . $row[code] . "   -   " . $row[name] . "   -   " . $row[curriculum] . "   -   " . $row[load];
 			if( empty($row[syllabus]) ){echo "|  Sem Ementa <br>"; };
@@ -237,7 +134,7 @@ if($schedule_data[$i][start_hour] >=  $day_end[0] ){ // IF THE DISCIPLINE STARTS
 			foreach ($row as $columnName => $columnData) {
 
         		echo 'Column name: ' . $columnName . ' Column data: ' . $columnData . ' ';
-//echo '<br />';
+        		//echo '<br />';
 
    			}
 
